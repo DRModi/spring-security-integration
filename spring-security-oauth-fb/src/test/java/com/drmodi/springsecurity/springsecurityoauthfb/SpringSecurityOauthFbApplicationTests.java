35885c31cf0d90e6a67360ff76ba79a43bbee7db
Dixit Modi <dmodi.jmcs@gmail.com>
2020-03-04 20:21:41 -0500

    spring security - OAuth authentication

---
+++ spring-security-oauth-fb/src/test/java/com/drmodi/springsecurity/springsecurityoauthfb/SpringSecurityOauthFbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drmodi.springsecurity.springsecurityoauthfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthFbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
